848adb55c730e259b25baa2826a78ee751656a38
Gaurav Patil <gaurvpatil17@gmail.com>
2021-10-02 23:21:16 +0530

    create react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="grid-contaainer">
+
+{/* header  */}
+   <header>
+     <Link to="">React Shopping Cart </Link>
+   </header>
+
+{/* main  */}
+   <main>
+Product List
+   </main>
+
+
+{/* footer  */}
+   <footer>
+All rights reserved 
+   </footer>
+
+
     </div>
+  
   );
 }
 
