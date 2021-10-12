6e6e355ac7eff81bfb79b21e042e878b80e1d0f0
Gaurav Patil <gaurvpatil17@gmail.com>
2021-10-12 14:25:35 +0530

    changes for LINK

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Cart from "./components/Cart";
 import Filter from "./components/Filter";
 import Products from "./components/Products";
